e4a34dce68d99e08deb402beabc68ec0b02dfd60
Andre Gideao <andrejudiao@gmail.com>
2026-07-05 10:44:54 +0100

    Ver detalhes do produto funcional

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -2,7 +2,6 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { useCart } from "../contexts/CartContext";
 import { useEffect, useState } from "react";
 import type { Product } from "../types";
-import { dummyProducts } from "../assets/assets";
 import { Loading } from "../components/Loading";
 import {
   ArrowLeftIcon,
@@ -17,6 +16,8 @@ import {
 } from "lucide-react";
 import DummyReviewsSection from "../assets/DummyReviewsSection";
 import { ProductCard } from "../components/ProductCard";
+import api from "../config/api";
+import toast from "react-hot-toast";
 
 export function ProductPage() {
   const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "AOA";
@@ -34,11 +35,20 @@ export function ProductPage() {
     setLoading(true);
     setLocalQuantity(1);
     window.scrollTo(0, 0);
-    const product = dummyProducts.find((p) => p.id === id);
-    setProduct(product!);
-    setRelatedProducts(dummyProducts.filter((p) => p.id !== id));
-    setLoading(false);
-  }, [id, navigate]);
+    
+    api.get(`/products/${id}`).then(({data}) => {
+      setProduct(data.product);
+      return api.get(`/products?category=${data.product.category}`);
+    }).then(({data}) => {
+      setRelatedProducts(data.products.filter((p: Product) => p.id !== product?.id));
+      setLoading(false);
+    }).catch((error: any) => {
+      toast.error((error.response?.data?.message || error.message || "Erro ao carregar produto."));
+      navigate("/products");
+
+    }).finally(() => setLoading(false));
+
+  }, [id, navigate, product?.id]);
 
   if (loading) return <Loading />;
   if (!product) return null;
